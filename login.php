ee0bf43e827d4b1cb1379b9dc61ff151080e896f
AnandaHike <62773469+AnandaHike@users.noreply.github.com>
2021-04-06 19:14:18 -0300

    Update login.php

--- login.php
+++ login.php
@@ -8,7 +8,7 @@ $email = $_POST['email'];
 $senha = $_POST['senha'];
 
                                   
-$sql = "SELECT * FROM usuario WHERE email = '${email}' AND senha = '${senha}';";
+$sql = "SELECT * FROM usuario WHERE nm_email = '${email}' AND nm_senha = '${senha}';";
 
 
 $query = $db->query($sql);
@@ -16,7 +16,7 @@ $query = $db->query($sql);
 foreach($db->query($sql)as $row)
 
 if ($row>0){
-	$_SESSION['usuario'] = $row['email'];
+	$_SESSION['nome'] = $row['email'];
         //desconectar
         mysqli_close($db);
         header("Location: areaUsuario.php");
