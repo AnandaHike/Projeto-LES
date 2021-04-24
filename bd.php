26d2763b7eb16ac92aadcaf84ed00c660f465329
AnandaHike <nandabrunetti@hotmail.com>
2021-04-24 11:08:56 -0300

    Update bd.php

--- bd.php
+++ bd.php
@@ -1,12 +1,12 @@
 <?php
 
 $BDconexao = pg_connect("host=ec2-3-211-37-117.compute-1.amazonaws.com dbname=d80reggh2q0cv7 user=afvottstdygeph 
-password=78a9d9712ebfd5e440d2ff4a1d5cfd7efa209e58b3cc18e22f3b80b327414486 port=5432");
+password=78a9d9712ebfd5e440d2ff4a1d5cfd7efa209e58b3cc18e22f3b80b327414486 port=5432") or die ("Não conectou");
 
-if (!$BDconexao) {
- echo "Error: " . pg_connect_error();
- exit();
-}
-  pg_set_charset($BDconexao,"utf8");
+//if (!$BDconexao) {
+ //echo "Error: " . pg_connect_error();
+ //exit();
+//}
+// pg_set_charset($BDconexao,"utf8");
 
 ?>
